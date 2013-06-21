b7b9b7d86d8e5416a2fdb1bc7bd3671da2454c4d
laboshinl <laboshinl@gmail.com>
2013-06-21 14:23:25 +0400

    staging

--- template.py
+++ template.py
@@ -1,4 +1,5 @@
-import os, json
+import os
+import json
 from netaddr import IPNetwork
 from random import randint, choice
 
@@ -19,35 +20,24 @@ cft = CloudFormationTemplate(description=str(nodes_count)+"-nodes "+ image+" clu
 
 #Randomly generate ip-address
 ip=IPNetwork(choice(["10."+str(randint(0,255))+".","172.16.","192.168."])+str(randint(0,255))+".0/24")
+
 ip_list = list(ip)
 
-cft.parameters.add(Parameter('KeyName', 'String',
-    {
-        'Description': 'Name of an existing KeyPair',
-	'Default': keypair
-    })
-)
+cft.parameters.add(Parameter('KeyName', 'String',{
+        					 'Description': 'Name of an existing KeyPair',
+							 'Default': keypair}))
 
-cft.parameters.add(Parameter('ImageName', 'String',
-    {
-        'Description': 'Name of image to work with',
-	'Default': image
-    })
-)
+cft.parameters.add(Parameter('ImageName', 'String',{
+        					 'Description': 'Name of image to work with',
+							 'Default': image}))
 
-cft.parameters.add(Parameter('Flavor', 'String',
-    {
-        'Description': 'Instance size',
-	'Default': flavor
-    })
-)
+cft.parameters.add(Parameter('Flavor', 'String',{
+        					 'Description': 'Instance size',
+							 'Default': flavor}))
 
-cft.parameters.add(Parameter('ExtNetUuid', 'String',
-    {
-        'Description': 'UUID of the external network to be used for external access',
-	'Default': ext_net_uuid
-    })
-)
+cft.parameters.add(Parameter('ExtNetUuid', 'String',{
+        					 'Description': 'UUID of the external network to be used for external access',
+							 'Default': ext_net_uuid}))
 
 cft.resources.add(Resource("AllowAllSecurityGroup", 'AWS::EC2::SecurityGroup',
     {"GroupDescription" : "Open all ports, enable ping",
@@ -73,45 +63,32 @@ cft.resources.add(Resource("AllowAllSecurityGroup", 'AWS::EC2::SecurityGroup',
 				]
     })
 )
-cft.resources.add(Resource("Network", 'OS::Quantum::Net',
-    {
-        "name" : "LocalNetwork"
-    })
-)
-cft.resources.add(Resource("Subnet", 'OS::Quantum::Subnet',
-    {
-    "network_id" : { "Ref" : "Network" },
-	"ip_version" : 4,
-	"cidr" : str(ip),
-	"value_specs": {
-	"dns_nameservers": [dns_nameserver]
-	}
-    })
-)
+cft.resources.add(Resource("Network", 'OS::Quantum::Net',{
+        				   "name" : "LocalNetwork"}))
+        				   
+cft.resources.add(Resource("Subnet", 'OS::Quantum::Subnet',{
+					       "network_id" : { "Ref" : "Network" },
+						   "ip_version" : 4,
+						   "cidr" : str(ip),
+						   "value_specs": {
+						   "dns_nameservers": [dns_nameserver]}}))
 
 if create_new_router :
 	cft.resources.add(Resource("Router", 'OS::Quantum::Router'))
-	cft.resources.add(Resource("RouterExternalInterface", 'OS::Quantum::RouterGateway',
-	    {
-		"router_id" : { "Ref" : "Router"},
-		"network_id" : { "Ref" : "ExtNetUuid" }
-	    })
-	)
+	
+	cft.resources.add(Resource("RouterExternalInterface", 'OS::Quantum::RouterGateway',{
+							   "router_id" : { "Ref" : "Router"},
+							   "network_id" : { "Ref" : "ExtNetUuid" }}))
 else:
-	cft.parameters.add(Parameter('Router', 'String',
-    {
-        'Description': 'Existing router ID',
-		'Default': existing_router_uuid
-    })
-	)
-cft.resources.add(Resource("RouterInternalInterface", 'OS::Quantum::RouterInterface',
-    {
-	"router_id": { "Ref" : "Router" },
-	"subnet_id": { "Ref" : "Subnet" }
-    })
-	)
+	cft.parameters.add(Parameter('Router', 'String',{
+        						 'Description': 'Existing router ID',
+								 'Default': existing_router_uuid}))
+								 
+cft.resources.add(Resource("RouterInternalInterface", 'OS::Quantum::RouterInterface',{
+						   "router_id": { "Ref" : "Router" },
+						   "subnet_id": { "Ref" : "Subnet" }}))
 
-if create_new_router :
+if create_new_router:
 	cft.resources["RouterInternalInterface"]["DependsOn"] = "RouterExternalInterface"
 
 nodenames=""
@@ -120,76 +97,58 @@ for y in range(1,nodes_count+1):
 	nodenames=nodenames+str(ip_list[y*3])+"  node"+str(y)+"\n"
 
 for x in range(1,nodes_count+1):
-	cft.resources.add(Resource("port"+str(x), 'OS::Quantum::Port', 
-	{
-		"network_id": { "Ref" : "Network" },
-		"security_groups" : [{"Fn::GetAtt" : [ "AllowAllSecurityGroup", "ID" ]}],
-		"fixed_ips": [{
-			"subnet_id": { "Ref" : "Subnet" },
-			"ip_address": str(ip_list[x*3])
-		}]
-	})
-	)
+	cft.resources.add(Resource("port"+str(x), 'OS::Quantum::Port',{
+							   "network_id": { "Ref" : "Network" },
+							   "security_groups" : [{"Fn::GetAtt" : [ "AllowAllSecurityGroup", "ID" ]}],
+							   "fixed_ips": [{
+							   "subnet_id": { "Ref" : "Subnet" },
+							   "ip_address": str(ip_list[x*3])}]}))
+							   
 	cft.resources["port"+str(x)]["DependsOn"] = "AllowAllSecurityGroup"
 	if x==1:
-		cft.resources.add(Resource("FloatingIp"+str(x), 'OS::Quantum::FloatingIP', 
-		{
-			"floating_network_id" : { "Ref" : "ExtNetUuid" }
-		})
-		)
-		cft.resources.add(Resource("FloatingIpAssociate"+str(x), 'OS::Quantum::FloatingIPAssociation', 
-		{
-			"floatingip_id": { "Ref" : "FloatingIp"+str(x) },
-			"port_id": { "Ref" : "port"+str(x) }
-		})
-		)
+		cft.resources.add(Resource("FloatingIp"+str(x), 'OS::Quantum::FloatingIP',{
+								   "floating_network_id" : { "Ref" : "ExtNetUuid" }}))
+								   
+		cft.resources.add(Resource("FloatingIpAssociate"+str(x), 'OS::Quantum::FloatingIPAssociation',{
+								   "floatingip_id": { "Ref" : "FloatingIp"+str(x) },
+								   "port_id": { "Ref" : "port"+str(x) }}))
+								   
 		cft.resources["FloatingIpAssociate"+str(x)]["DependsOn"] = "port"+str(x)
-		if create_new_router :
+		
+		if create_new_router:
 			cft.resources["FloatingIpAssociate"+str(x)]["DependsOn"] = "RouterInternalInterface"
-	cft.resources.add(Resource("node"+str(x), 'AWS::EC2::Instance',
-    	{
-        	'ImageId': { 'Ref' : 'ImageName' },
-        	'InstanceType': { 'Ref' : 'Flavor' },
-		'KeyName' : { 'Ref' : 'KeyName' },
-        	'UserData': base64(base64(open('init.sh').read())),
-		'NetworkInterfaces': [{ "Ref" : "port"+str(x)}]
-    	})
-	)
+			
+	cft.resources.add(Resource("node"+str(x), 'AWS::EC2::Instance',{
+        					   'ImageId': { 'Ref' : 'ImageName' },
+        					   'InstanceType': { 'Ref' : 'Flavor' },
+							   'KeyName' : { 'Ref' : 'KeyName' },
+        					   'UserData': base64(base64(open('init.sh').read())),
+							   'NetworkInterfaces': [{ "Ref" : "port"+str(x)}]}))
+							   
 	runlist = "node"+str(x)+'.json' if os.path.exists('node'+str(x)+'.json') else "default.json" 
 	cft.resources["node"+str(x)]['Metadata'] = {"AWS::CloudFormation::Init" : {
-        			"config" : {
-					"commands" : {
-					    "hosts" : {
-						"command" : "echo $'" + nodenames + "' >> /etc/hosts"
-					    },
-					    "hostname" : {
-						"command" : "hostname node" + str(x)
-					    },
- 					    "hostname2" : {
-						"command" : "sed -ri 's#^HOSTNAME=.*#HOSTNAME=node" + str(x) + "#' /etc/sysconfig/network"
-					    }
-					},
-		        		"files" : {
-					      	"/etc/chef/client.rb" : {
-							"content" : base64(open('client.rb').read()),
-					        	"mode" : "000644",
-					        	"owner" : "root",
-					        	"group" : "root"
-					      	},
-					      	"/etc/chef/runlist.json": {
-					      		"content" : json.loads(open(runlist).read()),
-					        	"mode" : "000644",
-					        	"owner" : "root",
-					        	"group" : "root"
-					      	},
-					      	"/etc/chef/validation.pem" : {
-				        		"content" : base64(open('validation.pem').read()),
-				        		"mode" : "000644",
-				        		"owner" : "root",
-				        		"group" : "root"
-				      		}
-	              	}	
-        		}
-        	}
-		}
+        										"config" : {
+												"commands" : {
+					    						"hosts" : {
+												"command" : "echo $'" + nodenames + "' >> /etc/hosts"},
+					    						"hostname" : {
+												"command" : "hostname node" + str(x)},
+ 					    						"hostname2" : {
+												"command" : "sed -ri 's#^HOSTNAME=.*#HOSTNAME=node" + str(x) + "#' /etc/sysconfig/network"}},
+		        								"files" : {
+					      						"/etc/chef/client.rb" : {
+												"content" : base64(open('client.rb').read()),
+					        					"mode" : "000644",
+					        					"owner" : "root",
+					        					"group" : "root"},
+					      						"/etc/chef/runlist.json": {
+					      						"content" : json.loads(open(runlist).read()),
+					        					"mode" : "000644",
+					        					"owner" : "root",
+					        					"group" : "root"},
+					      						"/etc/chef/validation.pem" : {
+				        						"content" : base64(open('validation.pem').read()),
+				        						"mode" : "000644",
+				        						"owner" : "root",
+				        						"group" : "root"}}}}}
 
